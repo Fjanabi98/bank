ede2959f680e3d90a278c93b5ca58aeb8a67b6a3
Fahad Janabi <Fahad.janabi98@yahoo.com>
2022-09-23 15:15:48 -0400

    bank

---
+++ bank/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+
+        public void deposit (double amount)
+        {
+            Console.WriteLine($"simulating a deposit of (amount,0:c) to your account");
+            _balance = amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+       
+    }
+}
---
+++ bank/Program.cs
@@ -0,0 +1,22 @@
+﻿using bank;
+
+       BankAccount ba = new BankAccount();
+
+        Console.WriteLine("How much $$$ do you want to deposit?");
+        double amountToDeposit = double.Parse(Console.ReadLine());
+
+        ba.deposit(amountToDeposit);
+
+        double userBalance = ba.GetBalance();
+
+        Console.WriteLine($"your current balance is:{userBalance,0:c}");
+        
+
+
+      
+
+ 
+
+
+
+
